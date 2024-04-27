1486383e77a662ca37371af0756aea2cd1e35221
bla1st <roman.nedorezov@tktk.ee>
2024-04-27 17:43:14 +0300

    add UserGenderValidation

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Enter your age: ");
+int userAge = Int32.Parse(Console.ReadLine());
+
+if(userAge >= 13)
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+else  if(userAge < 13)
+{
+    Console.WriteLine("You are too young to use Instagram.");
+}
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Please, enter ypur last name: ");
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
+    
+
